77b02bb1e935a868bdc474ac75fadb1fcecbb699
shivam248 <39190164+shivam248@users.noreply.github.com>
2018-12-06 04:34:14 -0800

    Add files via upload

---
+++ js main.js
@@ -0,0 +1,10 @@
+$('view-work').on('click',function(){
+    const image = $('#images').position().top;
+
+    $('html,body').animate(
+        {
+            scrollTop:images
+        },
+        900
+    );
+});
